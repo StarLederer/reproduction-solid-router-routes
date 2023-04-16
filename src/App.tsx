334578eb3477ccc31eea08f2482544648c080b4e
Star Lederer <germans.lederers@gmail.com>
2023-04-16 17:04:39 +0200

    Update things

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,44 @@
-import Root from './tree/Root'
+import type { Component } from 'solid-js'
+import { Route, Router, Routes, useNavigate, useParams, A, Outlet } from '@solidjs/router'
 
-export default Root
+const Layout: Component = () => {
+  const navigate = useNavigate()
+  return (
+    <>
+      <nav>
+        <A href='/pg1'>Page 1</A>
+        <A href='/pg2'>Page 2</A>
+        <A href='/pg3'>Page 3</A>
+      </nav>
+
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
+
+const Page: Component = () => {
+  const { page } = useParams()
+
+  return (
+    <article>
+      <h1>{page}</h1>
+      Hello from {page}
+    </article>
+  )
+}
+
+const Main: Component = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" component={Layout} >
+          <Route path=":page" component={Page} />
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
+
+export default Main
--- src/lib/router/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Outlet } from './Outlet'
-export { default as Page } from './Page'
-export { default as spaIntegration } from './integration'
